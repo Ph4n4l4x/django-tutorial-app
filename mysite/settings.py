0be62c153f66d7de4a9c88f515c0732df221e208
Shuh Fei Chia <chi0001.14.33@gmail.com>
2021-03-19 22:41:19 +1100

    Fixed Secret Key import bug

--- mysite/settings.py
+++ mysite/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 from dotenv import load_dotenv
 load_dotenv()
+import os
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
